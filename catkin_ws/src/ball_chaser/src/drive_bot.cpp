f18f6f7e58021347d52a8016426a4f3910b2d34a
Fernando Soto <fernandojose.soto@gmail.com>
2019-06-19 16:18:11 -0700

    final corrections

--- catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -14,21 +14,20 @@ bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
     ball_chaser::DriveToTarget::Response& res)
 {
 
-    ROS_INFO("DriveToTargetRequest received - linear_x:%1.2f, angular_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
+    //ROS_INFO("DriveToTargetRequest received - linear_x:%1.2f, angular_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
 
     geometry_msgs::Twist motor_command;
 
     motor_command.linear.x = (float)req.linear_x;
     motor_command.angular.z = (float)req.angular_z;
 
-    motor_command_publisher.publish(motor_command);
-
-    // Wait 3 seconds for arm to settle
-    ros::Duration(3).sleep();
+    motor_command_publisher.publish(motor_command);  
 
     // Return a response message
-    res.msg_feedback = "Motor command set - linear_x: " + std::to_string(motor_command.linear.x) + " , angular_z: " + std::to_string(motor_command.angular.z);
-    ROS_INFO_STREAM(res.msg_feedback);
+    if ((motor_command.linear.x != 0.0)||(motor_command.angular.z != 0.0)) {
+      res.msg_feedback = "Motor command set - linear_x: " + std::to_string(motor_command.linear.x) + " , angular_z: " + std::to_string(motor_command.angular.z);
+      ROS_INFO_STREAM(res.msg_feedback);
+    }
 
     return true;
 }
--- catkin_ws/src/ball_chaser/src/process_image.cpp
+++ catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -27,41 +27,56 @@ void process_image_callback(const sensor_msgs::Image img)
     int white_right = 0;
     float lin_x = 0.0;
     float ang_z = 0.0;
+    bool white_found = false;
+
+    // std::cout << "Image format: " << img.encoding << '\n';
+    // Assuming format 'rgb8'
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
 
-    for (int i = 0; i < img.height; i++) {
-      for (int j = 0; j < img.step; j++) {
-        if (img.data[i*img.step+j] == white_pixel) {
-            if (j<=img.step/3)
+    for (int i = 0; i < img.height * img.step; i = i+3) {
+      
+	int col = i % img.step;
+
+	// Check RGB values
+        if ((img.data[i] == white_pixel)&&(img.data[i+1] == white_pixel)&&(img.data[i+2] == white_pixel)) {
+	    
+	    white_found = true;
+            if (col<=img.step/3) {
               white_left++;
-            if ((j>img.step/3)&&(j<=2*img.step/3)) 
+            } else if ((col>img.step/3)&&(col<=2*img.step/3)) { 
               white_center++;
-            if (j>2*img.step/3) 
+            } else if (col>2*img.step/3) {
               white_right++;
+	    }
+
+        }      
+
+	// No need to scan the whole image.
+	// Exit as soon as the first row with white is found
+        if ((col==img.step-3) && white_found) {
+          break;
         }
-      }
-      if ((white_left>0)||(white_center>0)||(white_right>0)) { 
-        ROS_INFO_STREAM("ball found");
-        break;
-      }
+        
     }
 
+    //lin_x = 2;
     if (white_left>white_right) {
       // Turn left
-      ang_z = 1.57/16.0;
+      ang_z = 1.57/8.0;
     } else if (white_left<white_right) { 
       // Turn right
-      ang_z = -1.57/16.0;
+      ang_z = -1.57/8.0;
     } else if (white_center > 0) {
       // Straight
       ang_z = 0.0;
       lin_x = 5.0;
     } else {
-      // Stop    
+      // Stop   
+      lin_x = 0.0; 
     }
 
     drive_robot(lin_x,ang_z);
@@ -79,7 +94,7 @@ int main(int argc, char** argv)
     client = n.serviceClient<ball_chaser::DriveToTarget>("/ball_chaser/command_robot");
 
     // Subscribe to /camera/rgb/image_raw topic to read the image data inside the process_image_callback function
-    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 1, process_image_callback);
+    ros::Subscriber sub1 = n.subscribe("/camera/rgb/image_raw", 2, process_image_callback);
 
     // Handle ROS communication events
     ros::spin();
